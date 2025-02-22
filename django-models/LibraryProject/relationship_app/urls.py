75f1654358ab6009c0aa750294aa859b09474943
unknown <mazenosama987654321@gmail.com>
2025-02-22 02:33:10 +0200

    hi

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 from .views import home
 from django.contrib.auth import views as auth_views
-from .views import register
+from .views import views.register
+
 
 urlpatterns = [
     path("", home, name="home"),
