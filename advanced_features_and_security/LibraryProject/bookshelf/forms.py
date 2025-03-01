0abdb2c5da741d4ba307585d53261afe60821fdc
unknown <mazenosama987654321@gmail.com>
2025-03-01 13:50:48 +0200

    hi

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Book
 
-class BookSearchForm(forms.Form):
+class ExampleForm(forms.Form):
     title = forms.CharField(
         max_length=100, 
         required=False, 
