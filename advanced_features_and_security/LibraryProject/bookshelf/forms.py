f2e16ecee8fc725c9a2873f7fb624a1f51e686a0
unknown <mazenosama987654321@gmail.com>
2025-03-01 13:50:02 +0200

    hi

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Book
+
+class BookSearchForm(forms.Form):
+    title = forms.CharField(
+        max_length=100, 
+        required=False, 
+        widget=forms.TextInput(attrs={"placeholder": "Search books..."})
+    )
