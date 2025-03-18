e1f9f5d1d8c4dd1aed4f08e2a664a25aed2a54d5
unknown <mazenosama987654321@gmail.com>
2025-03-18 13:07:58 +0200

    hi

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog_db',   # Your database name
+        'USER': 'django_user',      # MySQL username
+        'PASSWORD': 'your_password',# MySQL password
+        'HOST': 'localhost',        # MySQL server address
+        'PORT': '3306',             # Default MySQL port
     }
 }
 
