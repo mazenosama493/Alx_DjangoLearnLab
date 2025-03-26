b616af46d171064009639ee762d81704ec841c16
unknown <mazenosama987654321@gmail.com>
2025-03-26 09:44:53 +0200

    hi

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@84l1v_(eb^n_6burwyn0s!!_gh2jtm^od0!s_r@2w4fm@-y(p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
